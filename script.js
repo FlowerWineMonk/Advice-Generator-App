c15d890ebb9c9796228d4e3e8b932502af49da20
FlowerWineMonk <105585962+FlowerWineMonk@users.noreply.github.com>
2023-04-02 19:16:39 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
 const container = document.querySelector(".container");
+const advice_id = document.querySelector(".advice-id");
+const advice_generator = document.querySelector(".advice-generator");
 const renderNewAdviceBtn = document.querySelector(".icon-button");
 
 let advice;
 
-const adviceGeneratorDisplay = function (data) {
-  advice = `
-    <div class="container">
-      <p class = "advice-id">ADVICE #${data.id}</p>
-      <p class="advice-generator">${data.advice}</p>  
-      <svg class = "divider" width="444" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h196v1H0zM248 8h196v1H248z"/><g transform="translate(212)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg>
-    </div>
-    `;
-  container.innerHTML = "";
-  container.insertAdjacentHTML("beforeend", advice);
+const adviceGeneratorDisplay = function (id, text) {
+  advice_id.textContent = `ADVICE #${id}`;
+  advice_generator.textContent = text;
 };
 
 const adviceGeneratorFunction = async function () {
   try {
-    const response = await fetch(`https://api.adviceslip.com/advice`);
+    const response = await fetch("https://api.adviceslip.com/advice");
 
     if (!response)
       throw new Error(`Failed to show an advice ${response.status}`);
@@ -25,7 +20,9 @@ const adviceGeneratorFunction = async function () {
     const data = await response.json();
     console.log(data);
 
-    adviceGeneratorDisplay(data.slip);
+    const id = data.slip.id;
+    const text = data.slip.advice;
+    adviceGeneratorDisplay(id, text);
   } catch (err) {
     console.error(err);
   }
